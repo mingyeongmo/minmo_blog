15bcc338df3c1d2fe1b8b9d475fc2d9af464b299
mingyeongmo <mingyeongmp@naver.com>
2023-09-08 15:09:59 +0900

    프로젝트 셋팅

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+const { withContentlayer } = require("next-contentlayer");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  swcMinify: false,
+};
 
-module.exports = nextConfig
+module.exports = withContentlayer(nextConfig);
