1212ef6efc7c8ddfd96e1010416a8c732f682b10
mingyeongmo <mingyeongmp@naver.com>
2023-09-25 18:09:13 +0900

    검색 기능 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,54 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
+import type { InferGetStaticPropsType } from "next";
+import { ChangeEvent, useState } from "react";
+import { Post, allPosts } from "contentlayer/generated";
+import Image from "next/image";
+import PostList from "common/components/PostList/PostList";
+import { SearchImage } from "public/images";
+import styles from "./index.module.scss";
 
-const Home: NextPage = () => {
+const Home = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const [search, setSearch] = useState("");
+
+  console.log("post length : ", posts.length);
+
+  const inputSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value.toLowerCase());
+  };
+
   return (
-    <div>
-      <h1 className="title">
-        Welcome to <Link href="/blog">home</Link>
-      </h1>
+    <div className={styles.home}>
+      <h1>Blog</h1>
+      <div className={styles.search_container}>
+        <input
+          onChange={inputSearch}
+          className={styles.search_input}
+          type="text"
+          placeholder="검색어를 입력해주세요."
+        />
+
+        <Image className={styles.search_img} src={SearchImage} alt="돋보기" />
+      </div>
+      <PostList
+        posts={(posts as Post[]).filter(
+          (post) =>
+            post.title.toLowerCase().includes(search) ||
+            post.description.toLowerCase().includes(search)
+        )}
+      />
     </div>
   );
+};
+
+export const getStaticProps = async () => {
+  const posts = allPosts.sort(
+    (a, b) => Number(new Date(b.date)) - Number(new Date(a.date))
+  );
+
+  return {
+    props: {
+      posts,
+    },
+  };
 };
 
 export default Home;
---
+++ public/images/index.tsx
@@ -0,0 +1,5 @@
+import ProfileImage from "./profile2-dark.jpeg";
+import LightImage from "./light.svg";
+import SearchImage from "./search.svg";
+
+export { ProfileImage, LightImage, SearchImage };
