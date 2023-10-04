3d15de4a2663018ced465feb7beedf5ae96f16bd
mingyeongmo <mingyeongmp@naver.com>
2023-10-05 01:41:29 +0900

    각 카테고리 갯수에 따라 드롭다운 메뉴 순서 바꾸기

--- src/components/DropDown/DropDown.tsx
+++ src/components/DropDown/DropDown.tsx
@@ -1,9 +1,36 @@
-const DropDown = () => {
+import { InferGetServerSidePropsType, InferGetStaticPropsType } from "next";
+import { Post, allPosts } from "contentlayer/generated";
+import { getStaticProps } from "src/pages";
+
+const DropDown = ({
+  posts,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  // 카테고리 종류 (중복 O)
+  let categoryDupList = posts.map((post) => post.category);
+
+  // 각 카테고리의 갯수 구하기
+  let elementsNumber = categoryDupList.reduce((a, i) => {
+    return (a[i] = (a[i] || 0) + 1), a;
+  }, {});
+
+  // 갯수를 구한 객체를 내림차순으로 정렬
+  let ascObject = Object.fromEntries(
+    Object.entries(elementsNumber).sort(([, a], [, b]) => (a > b ? -1 : 1))
+  );
+
+  // 배열로 바꿔준다.
+  let ascArr = Object.keys(ascObject);
+
+  console.log({ categoryDupList });
+  console.log({ elementsNumber });
+  console.log({ ascObject });
+  console.log({ ascArr });
+
   return (
     <>
-      <li>TypeScript</li>
-      <li>React</li>
-      <li>일상</li>
+      {ascArr.map((post, index) => (
+        <li key={index}>{post}</li>
+      ))}
     </>
   );
 };
