b08cfcff18df2187a7d6005183e74e0b331cd345
mingyeongmo <mingyeongmp@naver.com>
2023-11-10 04:07:39 +0900

    코드는 항상 파일명에 맞게 짜여져야 한다고 생각하여, index 파일은 오로지 어떻게 배치 되었는지만 보이게 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,22 @@
 import type { InferGetStaticPropsType } from "next";
 import { useState } from "react";
 import { allPosts } from "@/contentlayer/generated";
-import { useSelector } from "react-redux";
-import { RootState } from "src/redux/configureStore";
 import {
   PostList,
   DropDown,
   SearchInput,
   Pagination,
 } from "src/components/Modules/Home";
-import Image from "next/image";
-import { DropdownImg, DropupImg } from "public/images";
 import styles from "./index.module.scss";
 
 const Home = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const category = useSelector((state: RootState) => {
-    return state.category;
-  });
-
-  const { cate, postLength } = category;
-
-  const [view, setView] = useState(false);
-
   const [currentPage, setCurrentPage] = useState(1);
 
   return (
     <div className={styles.home}>
       <h1>Blog</h1>
       <SearchInput />
-      <div className={styles.dropdown_menu}>
-        <div onClick={() => setView(!view)} className={styles.category}>
-          {cate ? cate : "전체"}
-          {view && <DropDown posts={posts} />}
-          {view ? (
-            <Image src={DropupImg} alt="drop-up" />
-          ) : (
-            <Image src={DropdownImg} alt="drop-down" />
-          )}
-          {postLength}
-        </div>
-      </div>
+      <DropDown posts={posts} />
       <PostList
         posts={posts}
         currentPage={currentPage}
