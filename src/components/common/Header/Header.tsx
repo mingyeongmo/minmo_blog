61500c682b4a3f383470782c037c208aca505ff8
mingyeongmo <mingyeongmp@naver.com>
2024-01-26 01:46:04 +0900

    메인 화면 반응형 1차 작업

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className={styles.navbar}>
         <div>
           <Link href="/" className={styles.nav_left}>
-            <Image src={LogoImg} alt={"로고"} width={52} height={50} />
+            <Image src={LogoImg} alt={"로고"} width={40} height={40} />
             <p>minmo</p>
           </Link>
         </div>
